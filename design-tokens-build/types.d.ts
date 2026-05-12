1078ef540dc0265a8ae8bd4ae2287b84c8defc3f
Michael Marszalek <mimarz@gmail.com>
2026-05-12 12:09:45 +0200

    tokens

--- design-tokens-build/types.d.ts
+++ design-tokens-build/types.d.ts
@@ -4,7 +4,10 @@ import type {} from '@digdir/designsystemet-types';
 // Augment types based on theme
 declare module '@digdir/designsystemet-types' {
   export interface ColorDefinitions {
+    primary: never;
     accent: never;
+    extra1: never;
+    extra2: never;
     neutral: never;
   }
   export interface SeverityColorDefinitions {
