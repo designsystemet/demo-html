0d03b4b23ef8ad35ac5497fc99a1f8b483e00f39
Michael Marszalek <mimarz@gmail.com>
2026-06-15 10:23:37 +0200

    moso

--- design-tokens-build/types.d.ts
+++ design-tokens-build/types.d.ts
@@ -5,17 +5,6 @@ import type {} from '@digdir/designsystemet-types';
 declare module '@digdir/designsystemet-types' {
   export interface ColorDefinitions {
     accent: never;
-    'main-color-2': never;
-    brand1: never;
-    brand2: never;
-    brand3: never;
-    'support-color-4': never;
-    'support-color-5': never;
-    'support-color-6': never;
-    'support-color-7': never;
-    'support-color-8': never;
-    'support-color-9': never;
-    'support-color-10': never;
     neutral: never;
   }
   export interface SeverityColorDefinitions {
